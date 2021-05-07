68bc1e17e21c358aa6470bb97ef7a7385f1d8b31
Jamall Shabazz <jsshabazz510@gmail.com>
2021-05-07 10:04:11 -0500

    add handlebars connection

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const Handlebars = require("handlebars");
-const template = Handlebars.compile("Name: {{name}}");
-console.log(template({ name: "Nils" }));
+var express = require('express');
+var exphbs  = require('express-handlebars');
 
+var app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.listen(3000);
 
