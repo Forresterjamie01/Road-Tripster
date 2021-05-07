6609199b41c4752d31337b9f9ebedd4a8e5341c7
John Baxter <juancho420@ymail.com>
2021-05-06 22:19:51 -0700

    relocated some routes to controllers/api and touched index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,9 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./userRoutes');
+const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-
-router.post('/', async (req, res) => {
-    try {
-      const userData = await User.create(req.body);
-      res.status(200).json(userData);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
 
 module.exports = router;
