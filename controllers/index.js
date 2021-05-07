cee6a231bb17d84f58b4050ebe9643edf78845f1
John Baxter <juancho420@ymail.com>
2021-05-06 17:09:06 -0700

    renamed routes to userRoutes and updated

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./routes');
+const homeRoutes = require('./userRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
