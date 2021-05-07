ed018395dab0bedb8165b4091ef7f8fe182be92c
Jamall Shabazz <jsshabazz510@gmail.com>
2021-05-06 22:59:12 -0500

    added login page

--- server.js
+++ server.js
@@ -1,14 +1,5 @@
-var btn = document.getElementById("btn");
-
-btn.addEventListener("click", function() {
-    var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json')
-    ourRequest.onload = function() {
-        var ourData = JSON.parse(ourRequest.responseText);
-        console.log(ourData[0]);
-    };
-    ourRequest.send();
-});
-
+const Handlebars = require("handlebars");
+const template = Handlebars.compile("Name: {{name}}");
+console.log(template({ name: "Nils" }));
 
 
