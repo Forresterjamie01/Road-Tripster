6f09d2029de40cf3959c1604bdb251697fe4722e
Jamall Shabazz <jsshabazz510@gmail.com>
2021-05-07 15:12:33 -0500

    add jss

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use(routes);
 
 app.get('/', function (req, res) {
-    res.render('home');
+    res.render('login');
 });
 
 app.listen(3000, () => {
