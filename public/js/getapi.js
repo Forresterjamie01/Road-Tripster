ce1560c63b10c89721ea510b3d2fab9ddfe02fb5
Forresterjamie01 <Forresterjamie01@yahoo.com>
2021-05-09 20:15:46 -0400

    updating api key for google maps search.

--- public/js/getapi.js
+++ public/js/getapi.js
@@ -2,5 +2,7 @@ console.log("JS loaded")
 
 $("#Submit").on("click", function(event){
     event.preventDefault();
-    console.log("On clic")
+    console.log("On click" "")
+
+    // Get Google API key, Copy sample code and try displaying google maps
 })
