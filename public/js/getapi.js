44a33b7f2c3e37b2287bd01bd7b33ce9a987e9d2
Jamall Shabazz <jsshabazz510@gmail.com>
2021-05-10 22:54:26 -0500

    added files

--- public/js/getapi.js
+++ public/js/getapi.js
@@ -1,4 +1,4 @@
-console.log("JS loaded")
+// console.log("JS loaded")
 
 //$("#Submit").on("click", function(event){
    // event.preventDefault();
@@ -6,11 +6,62 @@ console.log("JS loaded")
 
     // Get Google API key, Copy sample code and try displaying google maps
 //})
-var mylatlng = { lat: 37.989000, lng: -122.043390};
-var mapOptions = {
-    center: mylatlng,
-    zoom: 2,
-    mapTypeid: google.maps.mapTypeId.ROADMAP
-};
+// var mylatlng = { lat: 37.989000, lng: -122.043390};
+// var mapOptions = {
+//     center: mylatlng,
+//     zoom: 2,
+//     mapTypeid: google.maps.mapTypeId.ROADMAP
+// };
 
-var map = new google.maps.Map(document.getElementById("googleMap"),mapOptions)
+// var map = new google.maps.Map(document.getElementById("googleMap"),mapOptions)
+
+function initMap() {
+    var pointA = new google.maps.LatLng(51.7519, -1.2578),
+        pointB = new google.maps.LatLng(50.8429, -0.1313),
+        myOptions = {
+            zoom: 7,
+            center: pointA
+        },
+        map = new google.maps.Map(document.getElementById('map-canvas'), myOptions),
+        // Instantiate a directions service.
+        directionsService = new google.maps.DirectionsService,
+        directionsDisplay = new google.maps.DirectionsRenderer({
+            map: map
+        }),
+        markerA = new google.maps.Marker({
+            position: pointA,
+            title: "point A",
+            label: "A",
+            map: map
+        }),
+        markerB = new google.maps.Marker({
+            position: pointB,
+            title: "point B",
+            label: "B",
+            map: map
+        });
+
+    // get route from A to B
+    calculateAndDisplayRoute(directionsService, directionsDisplay, pointA, pointB);
+
+}
+
+
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay, pointA, pointB) {
+    directionsService.route({
+        origin: pointA,
+        destination: pointB,
+        avoidTolls: true,
+        avoidHighways: false,
+        travelMode: google.maps.TravelMode.DRIVING
+    }, function (response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+        } else {
+            window.alert('Directions request failed due to ' + status);
+        }
+    });
+}
+
+initMap();
